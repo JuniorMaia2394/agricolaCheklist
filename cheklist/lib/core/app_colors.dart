dbaedc47133002f189958b4ed914009d687a1231
helioVictor <heliohelio1@outlook.com>
2021-06-10 20:47:07 -0300

    Adiciona estilos ao projeto

---
+++ cheklist/lib/core/app_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color white = Color(0xFFFFFFFF);
+  static final Color black = Color(0xFF000000);
+  static final Color font = Color(0xFF121213);
+  
+  static final Color lightGray = Color(0xFFEDEDED);
+  static final Color gray = Color(0xFFBBBBBB);
+  static final Color darkGray = Color(0xFF434343);
+  
+  static final Color lightPrimary = Color(0xFF408CFF);
+  static final Color primary = Color(0xFF277EFF);
+  static final Color darkPrimary = Color(0xFF0D6EFF);
+  
+  static final Color lightSuccess = Color(0xFF20D370);
+  static final Color success = Color(0xFF0BD465);
+  static final Color darkSuccess = Color(0xFF00FF73);
+  
+  static final Color lightDanger = Color(0xFFD32020);
+  static final Color danger = Color(0xFFD30B0B);
+  static final Color darkDanger = Color(0xFFFF0000);
+
+  static final Color lightAlert = Color(0xFFCFD320);
+  static final Color alert = Color(0xFFCFD30B);
+  static final Color darkAlert = Color(0xFFFAFF00);
+}
---
+++ cheklist/lib/core/app_text_styles.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'app_colors.dart';
+
+class AppTextStyles {
+  static final TextStyle navTitle = GoogleFonts.inter(
+    color: AppColors.white,
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+  );
+
+  static final TextStyle title = GoogleFonts.inter(
+    color: AppColors.font,
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle label = GoogleFonts.inter(
+    color: AppColors.white,
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+  );
+}
