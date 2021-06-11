c53b1ae780d016e08d47af5c1b583742d25f3afc
Junior Maia <maiajr2394@gmail.com>
2021-06-10 21:05:58 -0300

    Imagens
    Adicionado imagens para o checklist de manutenção, e criado classe app_images.dart

---
+++ cheklist/lib/core/app_images.dart
@@ -0,0 +1,24 @@
+class AppImages {
+  static String get nivelDeOleo => "assets/images/1.png";
+  static String get nivelOleoHidraulico => "assets/images/2.png";
+  static String get nivelOleoTransmissao => "assets/images/3.png";
+  static String get nivelOleoFreio => "assets/images/4.png";
+  static String get nivelLiquidoArrefecedor => "assets/images/5.png";
+  static String get correiaTensor => "assets/images/6.png";
+  static String get aguaReservatorio => "assets/images/7.png";
+  static String get solucaoLimpeza => "assets/images/8.png";
+  static String get capasProtetoras => "assets/images/9.png";
+  static String get realizarLubrificacao => "assets/images/10.png";
+  static String get suspirosCaixa => "assets/images/11.png";
+  static String get pneusCalibragem => "assets/images/12.png";
+  static String get fiosSoltos => "assets/images/13.png";
+  static String get folgasRodas => "assets/images/14.png";
+  static String get instrumentosPainel => "assets/images/15.png";
+  static String get folgaPedal => "assets/images/16.png";
+  static String get acionamentoAlavancas => "assets/images/17.png";
+  static String get luzesServico => "assets/images/18.png";
+  static String get drenagem => "assets/images/19.png";
+  static String get inspecao360 => "assets/images/20.png";
+  static String get controlesNormal => "assets/images/21.png";
+  static String get verificarSeguranca => "assets/images/22.png";
+}
