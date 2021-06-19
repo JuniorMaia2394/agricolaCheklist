3d5a77883c45f362db528d24b388091e7038df86
helioVictor <heliohelio1@outlook.com>
2021-06-19 20:00:49 -0300

    Soluciona o retorno nulo (#19)

--- cheklist/lib/home/Widgets/card/form_card.dart
+++ cheklist/lib/home/Widgets/card/form_card.dart
@@ -1,7 +1,9 @@
-import 'package:cheklist/home/Widgets/card/card_full.dart';
+import 'package:cheklist/core/app_colors.dart';
+import 'package:cheklist/core/app_text_styles.dart';
+
 import 'package:flutter/material.dart';
 
-class FormCard extends StatelessWidget {
+class FormCard extends StatefulWidget {
   final String title;
   final String cardNumber;
 
@@ -12,7 +14,117 @@ class FormCard extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _FormCardState createState() => _FormCardState();
+}
+
+class _FormCardState extends State<FormCard> {
+  var _cardBorderColor = AppColors.white;
+
+  var _successBorderColor = AppColors.success;
+  var _successButtonBackgroundColor = AppColors.success;
+  
+  var _dangerBorderColor = AppColors.danger;
+  var _dangerButtonBackgroundColor = AppColors.danger;
+
+  @override
   Widget build(BuildContext context) {
-    return CardFull();
+
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+        side: BorderSide(
+          color: _cardBorderColor,
+          width: 2.0,
+        ),
+      ),
+      shadowColor: AppColors.black.withOpacity(1),
+      child: Column(
+        children: <Widget>[
+          Row(children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Text(
+                widget.cardNumber,
+                style: AppTextStyles.cardNumber,
+              ),
+            ),
+            Text(
+              widget.title,
+              style: AppTextStyles.label,
+            ),
+          ]),
+          Icon(
+            Icons.agriculture,
+            size: 100,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: SizedBox(
+                    width: 70,
+                    height: 40,
+                    child: ElevatedButton(
+                      child: Text('OK', style: AppTextStyles.button),
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(_successButtonBackgroundColor),
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                              side: BorderSide(
+                                color: _successBorderColor,
+                                width: 2.0,
+                              ),
+                            )
+                          ),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _cardBorderColor = AppColors.darkSuccess;
+                          _successBorderColor = AppColors.darkSuccess;
+                          _successButtonBackgroundColor = AppColors.lightSuccess;
+                          _dangerBorderColor = AppColors.lightDanger;
+                          _dangerButtonBackgroundColor = AppColors.lightDanger;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                  child: ElevatedButton(
+                    child: Text('DEFEITO', style: AppTextStyles.button),
+                    style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(_dangerButtonBackgroundColor),
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                              side: BorderSide(
+                                color: _dangerBorderColor,
+                                width: 2.0,
+                              ),
+                            )
+                          ),
+                      ),
+                    onPressed: () {
+                      setState(() {
+                        _cardBorderColor = AppColors.darkDanger;
+                        _successBorderColor = AppColors.lightSuccess;
+                        _successButtonBackgroundColor = AppColors.lightSuccess;
+                        _dangerBorderColor = AppColors.darkDanger;
+                        _dangerButtonBackgroundColor = AppColors.lightDanger;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
