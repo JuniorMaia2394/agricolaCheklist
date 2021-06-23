bf52ea1d6fe1bd4eae08679594c76813a892e222
Junior Maia <maiajr2394@gmail.com>
2021-06-23 16:43:20 -0300

    Pdf
    Criar arquivo para salvar as informações.
    Feito o titulo.

--- cheklist/lib/home/Widgets/form/custom_form.dart
+++ cheklist/lib/home/Widgets/form/custom_form.dart
@@ -1,8 +1,14 @@
+import 'dart:async';
+
 import 'package:cheklist/core/app_colors.dart';
 import 'package:cheklist/data/tractor_problems.dart';
 import 'package:cheklist/home/Widgets/input/form_input.dart';
 import 'package:cheklist/home/Widgets/card/form_card.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdf/pdf.dart';
+import 'package:cheklist/mobile.dart';
+
+import 'package:pdf/widgets.dart' as pw;
 
 class CustomForm extends StatefulWidget {
   @override
@@ -40,35 +46,44 @@ class CustomFormState extends State<CustomForm> {
               child: FormInput(label: 'Identificação do trator'),
             ),
             ListView.builder(
-                physics: NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: tractorProblemsData.length,
-                itemBuilder: (ctx, problem) => Padding(
-                      padding: const EdgeInsets.fromLTRB(16, 5, 16, 5),
-                      child: FormCard(
-                        title:
-                            tractorProblemsData.values.elementAt(problem).title,
-                        cardNumber:
-                            tractorProblemsData.values.elementAt(problem).id,
-                        image: tractorProblemsData.values
-                            .elementAt(problem)
-                            .imageURL,
-                      ),
-                    )),
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: tractorProblemsData.length,
+              itemBuilder: (ctx, problem) => Padding(
+                padding: const EdgeInsets.fromLTRB(16, 5, 16, 5),
+                child: FormCard(
+                  title: tractorProblemsData.values.elementAt(problem).title,
+                  cardNumber: tractorProblemsData.values.elementAt(problem).id,
+                  image: tractorProblemsData.values.elementAt(problem).imageURL,
+                ),
+              ),
+            ),
             Padding(
               padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
-              child: FloatingActionButton.extended(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0)),
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text('Enviando os dados'),
-                        backgroundColor: AppColors.darkPrimary));
-                  }
-                },
-                label: const Text('Confirmar'),
-                icon: Icon(Icons.thumb_up),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: _createPDF,
+                      child: Text("Salvar"),
+                    ),
+                  ),
+                  FloatingActionButton.extended(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0)),
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text('Enviando os dados'),
+                            backgroundColor: AppColors.darkPrimary));
+                      }
+                    },
+                    label: const Text('Confirmar'),
+                    icon: Icon(Icons.thumb_up),
+                  ),
+                ],
               ),
             ),
           ],
@@ -76,4 +91,23 @@ class CustomFormState extends State<CustomForm> {
       ),
     );
   }
+}
+
+Future<void> _createPDF() async {
+  PdfDocument document = PdfDocument();
+  final page = document.pages.add();
+
+  page.graphics.drawString(
+    "CEFAT - CENTRO DE TREINAMENTO AGRÍCOLA FAMOSA",
+    PdfStandardFont(PdfFontFamily.helvetica, 15, style: PdfFontStyle.bold),
+    bounds: Rect.fromLTRB(50, 0, 0, 0),
+  );
+
+  // page.graphics.drawString("Nome:",
+  //     PdfStandardFont(PdfFontFamily.courier, 10, style: PdfFontStyle.regular));
+
+  List<int> bytes = document.save();
+  document.dispose();
+
+  saveAndLaunchFile(bytes, 'Output.pdf');
 }
